73be3db123abc49a7afce1e95edd8b18c2e8c63d
mbyk <marubayashi@gmail.com>
2019-12-17 18:01:39 +0900

    add file

---
+++ 2/2.5.1.rb
@@ -0,0 +1,21 @@
+#!~/.rbenv/shims/ruby
+puts true
+
+puts 1
+puts 0
+puts -1
+
+puts 'true'
+puts 'false'
+puts ''
+
+def find_data
+    true
+end
+data = find_data
+
+if data
+    puts 'データがあります'
+else
+    puts 'データがありません'
+end
