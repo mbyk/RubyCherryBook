8920ef59dc525d4152a0c14a1906b94864a04291
mbyk <marubayashi@gmail.com>
2019-12-18 22:33:18 +0900

    add 2.6.rb

---
+++ 2/2.6.rb
@@ -0,0 +1,38 @@
+#!~/.rbenv/shims/ruby
+
+def add(a, b)
+    a + b
+end
+
+puts add(1, 2)
+
+def hello_world
+    "hello, world"
+end
+
+puts hello_world
+
+def _hello_world
+    "hello, world!"
+end
+
+puts _hello_world
+
+def _hello_world_2
+    "hello, world!"
+end
+
+puts _hello_world_2
+
+
+# def 2_hello_world_2
+#     "hello, world!"
+# end
+
+# puts 2_hello_world_2
+
+def あいさつする
+    "はろー"
+end
+
+puts あいさつする
